3a1aad20bcfc5f95a9597399d73355a7a649fef8
Antoine HARS <ahars@ippon.fr>
2016-05-31 17:47:02 +0200

    add binding port 8080 to spark-shell script

--- spark/run-spark-shell.sh
+++ spark/run-spark-shell.sh
@@ -1,9 +1,9 @@
-dir=`pwd`
 SPARK_VERSION="1.6.1"
 HADOOP_VERSION="2.6"
 
-docker build --rm -t ahars/spark $dir
+docker build --rm -t ahars/spark .
 docker run -t -i --rm \
+	-p 8080:8080 \
 	--name spark \
 	ahars/spark \
         spark-$SPARK_VERSION-bin-hadoop$HADOOP_VERSION/bin/spark-shell 
